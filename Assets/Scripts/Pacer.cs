25978f283798e4ce08046f217cdb58f8248430e4
Mitch Silberstein <mitchsilberstein@aol.com>
2018-03-21 12:31:03 -0500

    Added a Moving Pick Up Object
    Added a new pickup object that moves

---
+++ Assets/Scripts/Pacer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Pacer : MonoBehaviour {
+	public float speed = 5.0f; 
+	private float zMax = 7.5f;
+	private float zMin = -7.5f; //starting position
+	private int direction = 1; //positive to start
+
+	// Update is called once per frame
+	void Update () {
+		float zNew = transform.position.z + direction * speed * Time.deltaTime;
+
+		if (zNew >= zMax) {
+			zNew = zMax;
+			direction *= -1;
+		}
+		else if (zNew <= zMin){
+			zNew = zMin;
+			direction *= -1;
+		}
+
+		transform.position = new Vector3 (7.5f, 0.75f, zNew);
+	}
+}
